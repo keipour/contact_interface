d3bcb5bef2ae0afb4a0d742dd5b10e5338fef099
Azarakhsh Keipour <keipour@gmail.com>
2020-05-01 20:24:27 -0400

    Added the contact topics and callbacks

--- include/contact_interface/contact_interface.h
+++ include/contact_interface/contact_interface.h
@@ -6,25 +6,45 @@
 #include <std_msgs/Bool.h>
 #include <mavros_msgs/State.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <contact_interface/ContactCommand.h>
+#include <contact_interface/ContactStatus.h>
 
 class ContactInterfaceNode : public BaseNode 
 {
 private:
+  // enums
+  enum ContactStatus
+  {
+    None = contact_interface::ContactStatus::CONTACT_NONE,
+    Approaching = contact_interface::ContactStatus::CONTACT_APPROACH,
+    InContact = contact_interface::ContactStatus::CONTACT_CONTACT,
+    Departing = contact_interface::ContactStatus::CONTACT_DEPART
+  };
+
+  enum TaskStatus
+  {
+    NotReceived = contact_interface::ContactStatus::TASK_NONE,
+    InProgress = contact_interface::ContactStatus::TASK_IN_PROGRESS,
+    Completed = contact_interface::ContactStatus::TASK_COMPLETED,
+    Aborted = contact_interface::ContactStatus::TASK_ABORTED
+  };
+
   // variables
   bool is_offboard = false;
   bool is_armed = false;
 
   // subscribers
-  ros::Subscriber mode_sub, arm_sub, state_sub, pose_sub;
+  ros::Subscriber mode_sub, arm_sub, state_sub, pose_sub, contact_command_sub;
 
   // publishers
-  ros::Publisher pose_command_pub;
+  ros::Publisher pose_command_pub, contact_status_pub;
 
   // callbacks
   void mode_callback(const std_msgs::Bool::ConstPtr &msg);
   void arm_callback(const std_msgs::Bool::ConstPtr &msg);
   void pose_callback(const geometry_msgs::PoseStamped::ConstPtr &msg);
   void state_callback(const mavros_msgs::State::ConstPtr &msg);
+  void contact_command_callback(const contact_interface::ContactCommand::ConstPtr &msg);
 
 public:
   ContactInterfaceNode(std::string node_name);
--- src/contact_interface_node.cpp
+++ src/contact_interface_node.cpp
@@ -13,15 +13,19 @@ bool ContactInterfaceNode::initialize(){
   pose_sub = nh->subscribe<geometry_msgs::PoseStamped>("mavros/local_position/pose", 10, &ContactInterfaceNode::pose_callback, this);
   mode_sub = nh->subscribe<std_msgs::Bool>("has_control", 10, &ContactInterfaceNode::mode_callback, this);
   arm_sub = nh->subscribe<std_msgs::Bool>("is_armed", 10, &ContactInterfaceNode::arm_callback, this);
+  contact_command_sub = nh->subscribe<contact_interface::ContactCommand>("controller/contact/command", 10, &ContactInterfaceNode::contact_command_callback, this);
 
   // init publishers
   pose_command_pub = nh->advertise<geometry_msgs::PoseStamped>("pose_command", 10);
+  contact_status_pub = nh->advertise<geometry_msgs::PoseStamped>("controller/contact/status", 10);
 
   return true;
 }
 
 bool ContactInterfaceNode::execute()
 {
+  // ros::Time::now() - last_request > ros::Duration(5.0))
+  
   return true;
 }
 
@@ -40,6 +44,10 @@ void ContactInterfaceNode::pose_callback(const geometry_msgs::PoseStamped::Const
 }
 
 void ContactInterfaceNode::state_callback(const mavros_msgs::State::ConstPtr &msg)
+{
+}
+
+void ContactInterfaceNode::contact_command_callback(const contact_interface::ContactCommand::ConstPtr &msg)
 {
 }
 
